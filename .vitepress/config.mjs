cf6227006640956e7debdc90ae02dbab266df701
coderWenzi <coderzj@126.com>
2024-08-02 00:11:37 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,63 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'whatsApp系统指南',
+  base: '/docs/',
+  description: 'A VitePress Site',
+  themeConfig: {
+    outlineTitle: '文章目录',
+    outline: [2, 6],
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '教程', link: '/markdown-examples' },
+    ],
+
+    sidebar: [
+      {
+        text: 'Vue',
+        items: [
+          { text: 'vue入门', link: '/vue/vue入门' },
+          { text: 'vue精通', link: '/vue/vue精通' },
+        ],
+      },
+      {
+        text: 'React',
+        items: [
+          { text: 'react基础', link: '/react/react基础' },
+          { text: 'react入门', link: '/react/react入门' },
+          { text: 'react精通', link: '/react/react精通' },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+    ],
+
+    footer: {
+      copyright: 'Copyright@ 2024 Albert WenZi',
+    },
+
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档',
+          },
+          modal: {
+            noResultsText: '无法找到相关结果',
+            resetButtonTitle: '清除查询条件',
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+            },
+          },
+        },
+      },
+    },
+  },
+})
---
+++ index.md
@@ -0,0 +1,38 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: 'WhatsApp文档教程'
+  text: '解决疑惑、常见问题等'
+  tagline: 持续进步！Nice Nice
+  image:
+    src: /bg.svg
+    alt: 背景图
+  actions:
+    - theme: brand
+      text: 教程起步
+      link: /api-examples
+    - theme: alt
+      text: 系统官网
+      link: https://www.baidu.com
+
+features:
+  - title: 聚合聊天
+    details: 全渠道客户沟通平台，聚合在线聊天（Livechat）、WhatsApp、Facebook Messenger、Instagram、Telegram、Line、Email、Wechat
+
+  - title: 全渠道沟通
+    details: 通过在线聊天（Livechat）、Messenger、 WhatsApp、Telegram、 Instagram、Line、Email、WeChat等无缝销售、营销或服务，以获得统一的客户体验。
+
+  - title: 覆盖合适的人群
+    details: 通过标签对不同渠道的客户进行细分，完善联系人信息，并在客户偏好的渠道上与他们沟通。体现您对客户的关注，提升客户的忠诚度。
+
+  - title: 确保客户信息安全
+    details: 您可以让客户服务人员直接管理支持，而无需让他们登录您的商店或渠道账号。您还可以通过设置个人用户角色权限，来保护您的特定数据、敏感信息和功能不被滥用。
+
+  - title: 1 对 1 演示指导
+    details: 我们的技术专家提供 1 对 1 的产品操作演示，指导您和您的团队迅速掌握和使用。
+
+  - title: 7天 / 14小时技术支持
+    details: 提供每周 7 天，每天 14 小时的技术支持，确保及时有效地解决您可能在任何时候遇到的任何技术问题。
+---
